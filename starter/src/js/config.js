ef111005cc53e305f42c39ebdc4d4f962bb0427e
Ksenia <kseniamakeko@gmail.com>
2024-06-08 15:46:44 +0500

    Initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9ad6368c-f39a-47e8-9bd7-93bfe4edfd9f';
+export const MODAL_CLOSE_SEC = 2.5;
